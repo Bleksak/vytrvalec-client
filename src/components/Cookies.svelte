7649249f800a38289d750884b21283a1385ecf6a
Jiri Velek <jiri.velek7@protonmail.com>
2023-12-19 14:43:49 +0100

    Cookies - change true to string value

--- src/components/Cookies.svelte
+++ src/components/Cookies.svelte
@@ -1,18 +1,17 @@
 <script lang="ts">
-  import LL from "../translations/i18n-svelte";
-  import Button from "./Button.svelte";
-  import { browser } from '$app/environment';
+	import LL from '../translations/i18n-svelte';
+	import Button from './Button.svelte';
+	import { browser } from '$app/environment';
 
-  let cookiesAccepted = $state(browser ? window.localStorage.getItem('cookiesAccepted') : true);
+	let cookiesAccepted = $state(browser ? window.localStorage.getItem('cookiesAccepted') : true);
 
-  const acceptCookies = () => {
-    window.localStorage.setItem('cookiesAccepted', true);
-	cookiesAccepted = true;
-  }
-
+	const acceptCookies = () => {
+		window.localStorage.setItem('cookiesAccepted', 'true');
+		cookiesAccepted = true;
+	};
 </script>
 
-<div class="container" class:hidden={cookiesAccepted} >
+<div class="container" class:hidden={cookiesAccepted}>
 	<div class="cookies">
 		<h6>{$LL.cookies.title()}</h6>
 		<p>{$LL.cookies.description()}</p>
@@ -21,29 +20,30 @@
 </div>
 
 <style>
-    .container {
-        position: sticky;
-        bottom: 2em;
-        background-color: #FFF;
-        padding: 2rem;
-        display: flex;
-        align-items: center;
-        max-width: 50em;
-	    margin: 0 auto;
-        box-shadow: 5px 5px 10px #757575;
-    }
-    .cookies {
-	    text-align: justify;
-	    margin-right: 2em;
-    }
-    h6 {
-	    color: #000;
-    }
-    .hidden {
+	.container {
+		position: sticky;
+		bottom: 2em;
+		background-color: #fff;
+		padding: 2rem;
+		display: flex;
+		align-items: center;
+		max-width: 50em;
+		margin: 0 auto;
+		box-shadow: 5px 5px 10px #757575;
+	}
+	.cookies {
+		text-align: justify;
+		margin-right: 2em;
+	}
+	h6 {
+		color: #000;
+	}
+	.hidden {
 		display: none;
-    }
+	}
 
-    span {
-        margin-right: 2rem;
-    }
+	span {
+		margin-right: 2rem;
+	}
 </style>
+
