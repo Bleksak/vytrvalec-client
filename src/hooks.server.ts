2ba6afe2f0ceb012d68dcb3238c470b678dee266
Jiri Velek <jiri.velek7@protonmail.com>
2023-12-19 17:59:02 +0100

    hooks - change method signature

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,7 @@
-import {getCurrentUser} from "$actions/Auth";
+import { getCurrentUser } from "$actions/Auth";
 
-export async function handle({event, resolve}: any): Promise<any> {
-    //TODO
+export const handle = async ({ event, resolve }: any): Promise<any> => {
+    // TODO: ???
     const result = await getCurrentUser();
     if (result.type === 'success') {
         event.locals.user = result.response
