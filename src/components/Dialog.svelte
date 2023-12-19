0f9c5b892e6861a774765a527018ab4dc2472b42
Jiri Velek <jiri.velek7@protonmail.com>
2023-12-19 14:43:58 +0100

    Dialog - styles fix

--- src/components/Dialog.svelte
+++ src/components/Dialog.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
-  export let dialog: HTMLDialogElement;
+	export let dialog: HTMLDialogElement;
 
-  export function close() {
-    dialog.close();
-  }
-
+	export function close() {
+		dialog.close();
+	}
 </script>
 
 <dialog bind:this={dialog}>
-	<div>
-		<button on:click={close} name="close">
-			<img src="/images/icons/close.svg" alt="Close" />
-		</button>
+	<div class="wrapper">
+		<div class="controls">
+			<button on:click={close} type="button">
+				<img src="/images/icons/close.svg" alt="Close" />
+			</button>
+		</div>
 		<div class="inner">
 			<slot />
 		</div>
@@ -19,31 +20,33 @@
 </dialog>
 
 <style>
-    dialog {
-        top: 50%;
-        left: 50%;
-        -webkit-transform: translateX(-50%) translateY(-50%);
-        -moz-transform: translateX(-50%) translateY(-50%);
-        -ms-transform: translateX(-50%) translateY(-50%);
-        transform: translateX(-50%) translateY(-50%);
-    }
+	dialog[open] {
+		top: 50%;
+		left: 50%;
+		-webkit-transform: translateX(-50%) translateY(-50%);
+		-moz-transform: translateX(-50%) translateY(-50%);
+		-ms-transform: translateX(-50%) translateY(-50%);
+		transform: translateX(-50%) translateY(-50%);
+	}
 
-    div {
-        width: 500px;
-        background-color: white;
-        border-radius: 10px;
-    }
+	div.wrapper {
+		width: 500px;
+		background-color: white;
+		border-radius: 10px;
+	}
 
-    button {
-        float: right;
-        margin: 20px;
-    }
+	.controls {
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-end;
+		padding: 20px 20px 0;
+	}
 
-    img:hover {
-        cursor: pointer;
-    }
+	img:hover {
+		cursor: pointer;
+	}
 
-    .inner {
-        padding: 50px;
-    }
+	div.inner {
+		padding: 50px;
+	}
 </style>
