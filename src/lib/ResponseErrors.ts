9a8321934df33d8b1e1c2870ed03209af0338d29
Jiri Velek <jiri.velek7@protonmail.com>
2023-12-20 17:01:13 +0100

    Errors - add 'too_large' error type

--- src/lib/ResponseErrors.ts
+++ src/lib/ResponseErrors.ts
@@ -1,6 +1,12 @@
-export type ResponseError = 'blank' | 'invalid' | 'weak' | 'not_unique' | 'logged_in' | 'server_down';
+export type ResponseError =
+	| 'blank'
+	| 'invalid'
+	| 'weak'
+	| 'not_unique'
+	| 'logged_in'
+	| 'too_large'
+	| 'server_down';
 
 export type ResponseErrorMap<T> = {
-    [K in keyof T]?: Array<ResponseError>;
+	[K in keyof T]?: Array<ResponseError>;
 };
-
