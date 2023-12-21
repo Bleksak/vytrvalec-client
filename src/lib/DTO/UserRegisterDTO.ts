9689f2c033af4d2ec96824bad7254cb673b2da38
Jiri Velek <jiri.velek7@protonmail.com>
2023-12-21 20:21:46 +0100

    fix naming

--- src/lib/DTO/UserRegisterDTO.ts
+++ src/lib/DTO/UserRegisterDTO.ts
@@ -1,71 +1,73 @@
-import type { RegistrationErrorMap } from "./UserRegisterResponse";
+import type { RegistrationError } from './UserRegisterResponse';
 
 export type UserRegisterDTO = {
-    email: string
-    password: string
-    first_name: string
-    last_name: string
-    faculty: number
+	email: string;
+	password: string;
+	first_name: string;
+	last_name: string;
+	faculty: number;
 };
 
-export type UserRegisterReturn = {
-    type: 'dto',
-    value: UserRegisterDTO,
-} | {
-    type: 'error',
-    value: RegistrationErrorMap
-};
+export type UserRegisterReturn =
+	| {
+			type: 'dto';
+			value: UserRegisterDTO;
+	  }
+	| {
+			type: 'error';
+			value: RegistrationError;
+	  };
 
 export const formDataToUserRegisterDTO = (formData: FormData): UserRegisterReturn => {
-    const email = formData.get('email')?.toString();
-    let errors: RegistrationErrorMap = {};
+	const email = formData.get('email')?.toString();
+	let errors: RegistrationError = {};
 
-    if (email === undefined || email === '') {
-        errors['email'] = ['blank'];
-    }
+	if (email === undefined || email === '') {
+		errors['email'] = ['blank'];
+	}
 
-    const password = formData.get('password')?.toString();
+	const password = formData.get('password')?.toString();
 
-    if (password === undefined || password === '') {
-        errors['password'] = ['blank'];
-    }
+	if (password === undefined || password === '') {
+		errors['password'] = ['blank'];
+	}
 
-    const firstName = formData.get('first_name')?.toString();
+	const firstName = formData.get('first_name')?.toString();
 
-    if (firstName === undefined || firstName === '') {
-        errors['first_name'] = ['blank'];
-    }
+	if (firstName === undefined || firstName === '') {
+		errors['first_name'] = ['blank'];
+	}
 
-    const lastName = formData.get('last_name')?.toString();
+	const lastName = formData.get('last_name')?.toString();
 
-    if (lastName === undefined || lastName === '') {
-        errors['last_name'] = ['blank'];
-    }
+	if (lastName === undefined || lastName === '') {
+		errors['last_name'] = ['blank'];
+	}
 
-    const faculty = Number(formData.get('faculty')?.toString());
+	const faculty = Number(formData.get('faculty')?.toString());
 
-    if (faculty === Number.NaN || !Number.isInteger(faculty)) {
-        errors['faculty'] = ['invalid'];
-    }
+	if (faculty === Number.NaN || !Number.isInteger(faculty)) {
+		errors['faculty'] = ['invalid'];
+	}
 
-    const gdpr = formData.get('gdpr');
+	const gdpr = formData.get('gdpr');
 
-    if (gdpr === null || gdpr.toString() != '1') {
-        errors['gdpr'] = ['blank'];
-    }
+	if (gdpr === null || gdpr.toString() != '1') {
+		errors['gdpr'] = ['blank'];
+	}
 
-    if (Object.keys(errors).length !== 0) {
-        return { type: 'error', value: errors };
-    }
+	if (Object.keys(errors).length !== 0) {
+		return { type: 'error', value: errors };
+	}
 
-    return {
-        type: 'dto',
-        value: {
-            email: email!,
-            password: password!,
-            first_name: firstName!,
-            last_name: lastName!,
-            faculty: faculty
-        }
-    }
-}
+	return {
+		type: 'dto',
+		value: {
+			email: email!,
+			password: password!,
+			first_name: firstName!,
+			last_name: lastName!,
+			faculty: faculty
+		}
+	};
+};
