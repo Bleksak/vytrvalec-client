1d55994758231349d9d8f2841d6de44274573031
Jiri Velek <jiri.velek7@protonmail.com>
2024-01-11 00:35:23 +0100

    auth - add types to actions

--- src/routes/auth/+page.server.ts
+++ src/routes/auth/+page.server.ts
@@ -1,11 +1,12 @@
 import { login } from '$actions/Auth';
 import { formDataToUserLoginDTO } from '$lib/DTO/UserLoginDTO';
-import { fail, redirect, type RequestHandler } from '@sveltejs/kit';
+import { fail, redirect, type Action } from '@sveltejs/kit';
 import axios from 'axios';
 import { register } from '$actions/Auth';
 import { formDataToUserRegisterDTO } from '$lib/DTO/UserRegisterDTO';
+import { type Actions } from '@sveltejs/kit';
 
-const loginAction: RequestHandler = async ({ cookies, request }): Promise<any> => {
+const loginAction: Action = async ({ cookies, request }) => {
 	const loginDTO = formDataToUserLoginDTO(await request.formData());
 
 	if (loginDTO.type === 'error') {
@@ -20,19 +21,18 @@ const loginAction: RequestHandler = async ({ cookies, request }): Promise<any> =
 
 	const token = result.response.token;
 	cookies.set('jwt', token, { path: '/' });
-	axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 
-	return { success: true };
+	axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
 
-const logoutAction: RequestHandler = ({ cookies }): Promise<any> => {
+const logoutAction: Action = ({ cookies }) => {
 	cookies.delete('jwt', { path: '/' });
 	axios.defaults.headers.common.Authorization = null;
 
 	redirect(307, '/');
 };
 
-const registerAction: RequestHandler = async ({ request }): Promise<any> => {
+const registerAction: Action = async ({ request }) => {
 	const registerDTO = formDataToUserRegisterDTO(await request.formData());
 
 	if (registerDTO.type === 'error') {
@@ -44,11 +44,9 @@ const registerAction: RequestHandler = async ({ request }): Promise<any> => {
 	if (result.type === 'error') {
 		return fail(400, { register: result.errors });
 	}
-
-	return { status: 201 };
 };
 
-export const actions = {
+export const actions: Actions = {
 	login: loginAction,
 	logout: logoutAction,
 	register: registerAction
