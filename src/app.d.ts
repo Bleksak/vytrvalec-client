80061151a2dde49fac51f78f1365c5af1c49bed9
Jiri Velek <jiri.velek7@protonmail.com>
2024-01-11 00:29:01 +0100

    Types - add Locals as TS type

---
+++ src/app.d.ts
@@ -0,0 +1,20 @@
+import type { UserResponse } from '$lib/DTO/UserResponse';
+
+declare global {
+	declare namespace App {
+		interface Locals {
+			jwt: string?;
+			user: UserResponse;
+		}
+	}
+
+	declare interface String {
+		removeAccents(): string;
+	}
+}
+
+String.prototype.removeAccents = function () {
+	return this.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+};
+
+export {};
