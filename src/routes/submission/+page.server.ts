636db4cbdc92167c72a60ae46b643486a0b06b8a
Jiri Velek <jiri.velek7@protonmail.com>
2024-01-14 03:51:57 +0100

    Submission - fix types

--- src/routes/submission/+page.server.ts
+++ src/routes/submission/+page.server.ts
@@ -1,19 +1,21 @@
 import { createSubmission } from '$actions/Submission';
 import { formDataToSubmissionDTO } from '$lib/DTO/SubmissionDTO';
-import { fail, type RequestHandler } from '@sveltejs/kit';
+import { fail, type Actions, type Action } from '@sveltejs/kit';
 
-const createAction: RequestHandler = async ({ request }): Promise<any> => {
+const createAction: Action = async ({ request }) => {
 	let dto = formDataToSubmissionDTO(await request.formData());
+
 	if (dto.type === 'error') {
 		return fail(400, { submission: dto.value });
 	}
 
 	const result = await createSubmission(dto.value);
+
 	if (result.type === 'error') {
 		return fail(400, { submission: result.errors });
 	}
 };
 
-export const actions = {
+export const actions: Actions = {
 	create: createAction
 };
