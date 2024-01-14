16bea2ca282061c308fc69e47f569b8bd842edc8
Jiri Velek <jiri.velek7@protonmail.com>
2024-01-14 03:49:06 +0100

    Widget - add component

---
+++ src/components/Widget.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	let {
+		children,
+		title = '',
+		...props
+	} = $props<HTMLAttributes<any> & { children: Snippet; title: string }>();
+</script>
+
+<aside {...props}>
+	{#if title !== ''}
+		<header>
+			<h4>{title}</h4>
+		</header>
+	{/if}
+	{@render children()}
+</aside>
+
+<style>
+	header {
+		margin-top: calc(-2rem - 15px);
+		background: white;
+		width: fit-content;
+	}
+
+	aside {
+		margin-top: 30px;
+		border: 2px solid #dee2e6;
+		padding: 20px;
+		border-radius: 10px;
+	}
+</style>
