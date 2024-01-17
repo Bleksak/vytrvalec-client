e4aa043991ea67252ab18f0c0821ce98e4d71deb
Jiri Velek <jiri.velek7@protonmail.com>
2024-01-17 18:48:39 +0100

    Accordion - redo in svelte style

--- src/components/Accordion.svelte
+++ src/components/Accordion.svelte
@@ -1,38 +1,12 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
+	import { slide } from 'svelte/transition';
 
 	let { opened, children } = $props<{ opened: boolean; children: Snippet }>();
-
-	let accordion = $state<HTMLElement>();
-	let offsetHeight = $state<number>(0);
-	let maxOffsetHeight = $state<number>(0);
-
-	$effect(() => {
-		if (offsetHeight > 0 && maxOffsetHeight === 0) {
-			maxOffsetHeight = offsetHeight;
-		}
-	});
-
-	$effect(() => {
-		if (maxOffsetHeight > 0) {
-			accordion!.style.maxHeight = opened ? maxOffsetHeight + 'px' : '0';
-		}
-	});
 </script>
 
-<ul bind:this={accordion} bind:offsetHeight class:opened>
-	{@render children()}
-</ul>
-
-<style>
-	ul {
-		height: auto;
-		overflow: hidden;
-		scrollbar-width: none;
-		transition: 200ms all linear;
-	}
-
-	.opened {
-		overflow: auto;
-	}
-</style>
+{#if opened}
+	<ul transition:slide>
+		{@render children()}
+	</ul>
+{/if}
