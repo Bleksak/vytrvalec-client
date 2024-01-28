7831eb9054ac90f60cd85eea9c0f835d9b51b7ab
Jiri Velek <jiri.velek7@protonmail.com>
2024-01-28 18:37:00 +0100

    Results - add route

---
+++ src/routes/[[lang=lang]]/results/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import Select from '$components/FormComponent/Select.svelte';
+	import LL from '$translations/i18n-svelte';
+
+	const weekPickerKeys = [
+		$LL.results.week_picker[0](),
+		$LL.results.week_picker[1](),
+		$LL.results.week_picker[2](),
+		$LL.results.week_picker[3]()
+	];
+	const weekPickerValues = [-1, 0, 1, 2, 3];
+</script>
+
+<main>
+	<header>
+		<h1>{$LL.results.title()}</h1>
+	</header>
+
+	<section class="pickers">
+		<div class="picker-wrapper">
+			<Select id="week_picker" keys={weekPickerKeys} values={weekPickerValues} />
+		</div>
+
+		<div class="picker-wrapper">
+			<Select id="year_picker" keys={weekPickerKeys} values={weekPickerValues} />
+		</div>
+	</section>
+</main>
+
+<style>
+	main {
+		padding-inline: 100px;
+	}
+
+	.pickers {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+
+	.picker-wrapper {
+		width: 20%;
+	}
+</style>
