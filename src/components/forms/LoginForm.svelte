4c1367d6404843e067fbcc7f8ef2fc3b3fb080c0
Jiri Velek <jiri.velek7@protonmail.com>
2024-01-29 20:04:17 +0100

    Login & Register - fix errors, use form-field

--- src/components/forms/LoginForm.svelte
+++ src/components/forms/LoginForm.svelte
@@ -6,14 +6,19 @@
 	import LL from '$translations/i18n-svelte';
 	import type { SubmitFunction } from '@sveltejs/kit';
 	import type { HTMLDialogAttributes } from 'svelte/elements';
+	import type { LoginError } from '$lib/DTO/UserLoginResponse';
 
 	let { ...props } = $props<HTMLDialogAttributes>();
 	let dialog = $state<Dialog>();
+
+	let errors = $state<LoginError>();
 
 	const enhancer: SubmitFunction = () => {
 		return async ({ result, update }) => {
 			if (result.type === 'success') {
 				dialog?.close();
+			} else if (result.type === 'failure') {
+				errors = result.data?.login as LoginError;
 			}
 
 			update();
@@ -23,32 +28,39 @@
 
 <Dialog bind:this={dialog} header={$LL.login.title()} {...props}>
 	<form method="POST" action="/auth?/login" use:enhance={enhancer} name="login">
-		{#each $page?.form?.login ?? [] as error}
-			<span class="error">
-				{error}
-				<!-- TODO: tady musime vyresit jak vypisovat errory typu spatnej login -->
-				<!-- {$LL.login.errors.email[error as keyof typeof $LL.login.errors.email]()} -->
-			</span>
-		{/each}
-		<label for="email">
-			{$LL.login.email()}:
-		</label>
-		<input type="email" name="email" id="email" />
-		{#each $page?.form?.login?.email ?? [] as error}
-			<span class="error">
-				{$LL.login.errors.email[error as keyof typeof $LL.login.errors.email]()}
-			</span>
-		{/each}
+		<div class="form-field">
+			{#each errors?.auth ?? [] as error}
+				<span class="error">
+					{$LL.login.errors.auth[error as keyof typeof $LL.login.errors.auth]()}
+					<!-- TODO: tady musime vyresit jak vypisovat errory typu spatnej login -->
+					<!-- {$LL.login.errors.email[error as keyof typeof $LL.login.errors.email]()} -->
+				</span>
+			{/each}
+		</div>
 
-		<label for="password">
-			{$LL.login.password()}:
-		</label>
-		<input type="password" name="password" id="password" />
-		{#each $page?.form?.login?.password ?? [] as error}
-			<span class="error">
-				{$LL.login.errors.password[error as keyof typeof $LL.login.errors.password]()}
-			</span>
-		{/each}
+		<div class="form-field">
+			<label for="email">
+				{$LL.login.email()}:
+			</label>
+			<input type="email" name="email" id="email" />
+			{#each errors?.email ?? [] as error}
+				<span class="error">
+					{$LL.login.errors.email[error as keyof typeof $LL.login.errors.email]()}
+				</span>
+			{/each}
+		</div>
+
+		<div class="form-field">
+			<label for="password">
+				{$LL.login.password()}:
+			</label>
+			<input type="password" name="password" id="password" />
+			{#each errors?.password ?? [] as error}
+				<span class="error">
+					{$LL.login.errors.password[error as keyof typeof $LL.login.errors.password]()}
+				</span>
+			{/each}
+		</div>
 
 		<Button class="middle">{$LL.login.submit()}</Button>
 	</form>
--- src/components/forms/RegistrationForm.svelte
+++ src/components/forms/RegistrationForm.svelte
@@ -9,91 +9,107 @@
 	import type { HTMLDialogAttributes } from 'svelte/elements';
 	import Checkbox from '$components/FormComponent/Checkbox.svelte';
 	import Select from '$components/FormComponent/Select.svelte';
+	import type { RegistrationError } from '$lib/DTO/UserRegisterResponse';
 
 	let { ...props }: HTMLDialogAttributes = $props();
 	let dialog = $state<Dialog>();
 
 	let facultiesPromise = fetchFaculties();
 
+	let errors = $state<RegistrationError>();
+
 	const enhancer: SubmitFunction = () => {
 		return async ({ result, update }) => {
 			if (result.type === 'success') {
 				dialog?.close();
+			} else if (result.type === 'failure') {
+				errors = result?.data?.register as RegistrationError;
 			}
-
-			update();
 		};
 	};
 </script>
 
 <Dialog bind:this={dialog} header={$LL.registration.title()} {...props}>
 	<form method="POST" action="/auth?/register" use:enhance={enhancer}>
-		<label for="email">
-			{$LL.registration.email()}:
-		</label>
-		<input type="email" name="email" id="email" />
-		{#each $page?.form?.register?.email ?? [] as error}
-			<span class="error">
-				{$LL.registration.errors.email[error as keyof typeof $LL.registration.errors.email]()}
-			</span>
-		{/each}
+		<div class="form-field">
+			<label for="faculty">
+				{$LL.registration.faculty()}:
+			</label>
+			{#await facultiesPromise then faculties}
+				<Select
+					name="faculty"
+					id="faculty"
+					keys={faculties.map((f) => f.name)}
+					values={faculties.map((f) => f.id)}
+				/>
+			{:catch}
+				<span class="note">{$LL.registration.errors.faculty.no_faculties()}</span>
+			{/await}
+			{#each errors?.faculty ?? [] as error}
+				<span class="error">
+					{$LL.registration.errors.faculty[error as keyof typeof $LL.registration.errors.faculty]()}
+				</span>
+			{/each}
+		</div>
 
-		<label for="password">
-			{$LL.registration.password()}:
-		</label>
-		<input type="password" name="password" id="password" />
-		{#each $page?.form?.register?.password ?? [] as error}
-			<span class="error">
-				{$LL.registration.errors.password[error as keyof typeof $LL.registration.errors.password]()}
-			</span>
-		{/each}
+		<div class="form-field">
+			<label for="email">
+				{$LL.registration.email()}:
+			</label>
+			<input type="email" name="email" id="email" />
+			{#each errors?.email ?? [] as error}
+				<span class="error">
+					{$LL.registration.errors.email[error as keyof typeof $LL.registration.errors.email]()}
+				</span>
+			{/each}
+		</div>
 
-		<label for="first_name">
-			{$LL.registration.first_name()}:
-		</label>
-		<input type="text" name="first_name" id="first_name" />
-		{#each $page?.form?.register?.first_name ?? [] as error}
-			<span class="error">
-				{$LL.registration.errors.first_name[error as keyof typeof $LL.registration.errors.first_name]()}
-			</span>
-		{/each}
-		<label for="last_name">
-			{$LL.registration.last_name()}:
-		</label>
-		<input type="text" name="last_name" id="last_name" />
-		{#each $page?.form?.register?.last_name ?? [] as error}
-			<span class="error">
-				{$LL.registration.errors.last_name[error as keyof typeof $LL.registration.errors.last_name]()}
-			</span>
-		{/each}
+		<div class="form-field">
+			<label for="password">
+				{$LL.registration.password()}:
+			</label>
+			<input type="password" name="password" id="password" />
+			{#each errors?.password ?? [] as error}
+				<span class="error">
+					{$LL.registration.errors.password[error as keyof typeof $LL.registration.errors.password]()}
+				</span>
+			{/each}
+		</div>
 
-		<label for="faculty">
-			{$LL.registration.faculty()}:
-		</label>
-		{#await facultiesPromise then faculties}
-			<Select
-				name="faculty"
-				id="faculty"
-				keys={faculties.map((f) => f.name)}
-				values={faculties.map((f) => f.id)}
-			/>
-		{:catch}
-			<span class="note">{$LL.registration.errors.faculty.no_faculties()}</span>
-		{/await}
-		{#each $page?.form?.register?.faculty ?? [] as error}
-			<span class="error">
-				{$LL.registration.errors.faculty[error as keyof typeof $LL.registration.errors.faculty]()}
-			</span>
-		{/each}
+		<div class="form-field">
+			<label for="first_name">
+				{$LL.registration.first_name()}:
+			</label>
+			<input type="text" name="first_name" id="first_name" />
+			{#each errors?.first_name ?? [] as error}
+				<span class="error">
+					{$LL.registration.errors.first_name[error as keyof typeof $LL.registration.errors.first_name]()}
+				</span>
+			{/each}
+		</div>
 
-		<Checkbox id="gdpr" name="gdpr">
-			{$LL.registration.gdpr()}
-		</Checkbox>
-		{#each $page?.form?.register?.gdpr ?? [] as error}
-			<span class="error">
-				{$LL.registration.errors.gdpr[error as keyof typeof $LL.registration.errors.gdpr]()}
-			</span>
-		{/each}
+		<div class="form-field">
+			<label for="last_name">
+				{$LL.registration.last_name()}:
+			</label>
+			<input type="text" name="last_name" id="last_name" />
+			{#each errors?.last_name ?? [] as error}
+				<span class="error">
+					{$LL.registration.errors.last_name[error as keyof typeof $LL.registration.errors.last_name]()}
+				</span>
+			{/each}
+		</div>
+
+		<div class="form-field">
+			<Checkbox id="gdpr" name="gdpr">
+				{$LL.registration.gdpr()}
+			</Checkbox>
+			{#each errors?.gdpr ?? [] as error}
+				<span class="error">
+					{$LL.registration.errors.gdpr[error as keyof typeof $LL.registration.errors.gdpr]()}
+				</span>
+			{/each}
+		</div>
 		<Button class="middle">{$LL.registration.submit()}</Button>
 	</form>
 </Dialog>
