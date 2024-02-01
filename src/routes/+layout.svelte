6a370217129e9727d403dc41fbe8f6a9b386e684
Jiri Velek <jiri.velek7@protonmail.com>
2024-02-01 21:04:38 +0100

    Page layout - add toast to every page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,41 @@
 <script lang="ts">
+	import Toast from '$components/Toast.svelte';
+	import { createToastStore } from '$lib/stores/ToastStore.svelte';
+	import { setContext } from 'svelte';
+	import { fade } from 'svelte/transition';
+
 	String.prototype.removeAccents = function () {
 		return this.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 	};
+
+	const toastStore = createToastStore();
+	setContext('toastStore', toastStore);
 </script>
 
+{#if toastStore.toasts().length > 0}
+	<div class="toasts-anchor" transition:fade>
+		<div class="toasts">
+			{#each toastStore.toasts() as toast}
+				<Toast {toast} />
+			{/each}
+		</div>
+	</div>
+{/if}
 <slot />
+
+<style>
+	.toasts-anchor {
+		z-index: 9999;
+		position: fixed;
+
+		bottom: 10%;
+		right: 10%;
+	}
+	.toasts {
+		position: relative;
+
+		display: flex;
+		flex-direction: column-reverse;
+		gap: 20px;
+	}
+</style>
