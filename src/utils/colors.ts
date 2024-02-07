82a8800cda2f990b75a58cd48d63d298fca5340b
Alena Kašparová <a.kasp@seznam.cz>
2024-02-07 22:04:37 +0100

    feat: colors - tags color maps

---
+++ src/utils/colors.ts
@@ -0,0 +1,19 @@
+export const FacultyColorMap: Record<string, string> = {
+    "FAV": "#DBAC00",
+    "FDU": "#E63329",
+    "FEK": "#EE7202",
+    "FEL": "#064291",
+    "FF": "#00B5DD",
+    "FPE": "#8ABD24",
+    "FPR": "#A20E2A",
+    "FST": "#008FD0",
+    "FZS": "#009767",
+    "DEFAULT": "#90A4AE"
+}
+
+export const SubmissionStateColorMap: Record<string, string> = {
+    "ACCEPTED": "#28A745",
+    "REJECTED": "#DC3545",
+    "PENDING": "#FFC107",
+    "DEFAULT": "#90A4AE"
+}
