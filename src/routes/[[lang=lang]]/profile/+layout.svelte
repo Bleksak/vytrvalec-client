0e8ec4d8419b1fbe2045814088264861ca2eed9b
Jiri Velek <jiri.velek7@protonmail.com>
2024-02-08 21:15:02 +0100

    Profile - set context in layout

---
+++ src/routes/[[lang=lang]]/profile/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import createUserSubmissionStore from "$lib/stores/UserSubmissionsStore.svelte";
+	import { setContext } from "svelte";
+
+	const userSubmissionsStore = createUserSubmissionStore();
+	setContext('userSubmissionsStore', userSubmissionsStore);
+</script>
+
+<slot/>
