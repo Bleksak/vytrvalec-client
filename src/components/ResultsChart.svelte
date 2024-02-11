1279a80e0b177138a87724a18b115625d1f2b88d
Jiri Velek <jiri.velek7@protonmail.com>
2024-02-11 17:53:00 +0100

    ResultsChart - rename component

---
+++ src/components/ResultsChart.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import type { Faculty } from '$lib/DTO/Faculty';
+	import type { ResultRow } from '$lib/DTO/SeasonResultDTO';
+	import LL from '$translations/i18n-svelte';
+	import { FacultyColorMap } from '$utils/colors';
+	import type { ChartOptions, ChartData } from 'chart.js';
+	import Chart from 'chart.js/auto';
+
+    let { faculties, results } = $props<{ faculties: Faculty[], results: ResultRow[] }>();
+
+    $inspect(results);
+    const resultFaculties = results.map(result => faculties.find(faculty => faculty.id === result.faculty));
+    const labels = resultFaculties.map(result => result?.name);
+    const dataset = results.map(result => Number((result.distance / 1000).toFixed(1)));
+    const colors = resultFaculties.map(faculty => FacultyColorMap[faculty!.shortcut as keyof typeof FacultyColorMap] ?? FacultyColorMap.DEFAULT);
+
+    let chart = $state<HTMLCanvasElement>();
+
+    const data: ChartData<'bar'> = {
+        labels: labels,
+        datasets: [
+            {
+                label: $LL.results.distance(),
+                data: dataset,
+                backgroundColor:colors,
+            },
+        ],
+    };
+
+    const options: ChartOptions<'bar'> = {
+        responsive: true,
+        plugins: {
+            tooltip: {
+                callbacks: {
+                    label: (item) => item.dataset.label + ': ' + item.formattedValue + ' km'
+                }
+            },
+            legend: {
+                display: false
+            },
+        },
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        },
+    };
+
+    $effect(() => {
+        new Chart(chart!, {
+            type: 'bar',
+            data: data,
+            options: options,
+        });
+    });
+</script>
+
+<canvas bind:this={chart}></canvas>
