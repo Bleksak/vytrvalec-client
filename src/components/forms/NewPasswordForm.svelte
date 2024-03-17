1ab775eff7aca18c72f1a67bc18add86804d13f9
Jiri Velek <jiri.velek7@protonmail.com>
2024-03-17 19:07:08 +0100

    PasswordReset - remove todo comments

--- src/components/forms/NewPasswordForm.svelte
+++ src/components/forms/NewPasswordForm.svelte
@@ -7,27 +7,22 @@
 	import { getContext } from 'svelte';
 	import { goto } from '$app/navigation';
 	import type { ResetError } from '$lib/DTO/ResetPasswordDTO';
-    import { page } from '$app/stores'
+	import { page } from '$app/stores';
 	import Store from '$lib/enums/Stores';
 
-    const toastStore = getContext<ToastStore>(Store.TOAST_STORE);
-    const resetPasswordToken = $page.url.pathname.split('/reset-password/')[1];
+	const toastStore = getContext<ToastStore>(Store.TOAST_STORE);
+	const resetPasswordToken = $page.url.pathname.split('/reset-password/')[1];
 
 	let errors = $state<ResetError>();
 
 	const onSubmit: SubmitFunction = ({ submitter }) => {
 		submitter?.setAttribute('disabled', 'disabled');
 		return async ({ result, update }) => {
-			// NOTE: dočasně změněn retun na redirect protože mi ro přišlo snažší než 
-			// řešit proč goto is not going to
 			if (result.type === 'redirect') {
 				toastStore.add({
 					type: 'success',
 					message: $LL.reset.success()
 				});
-                errors = undefined;
-                // FIXME: goto not going to
-				// goto('/');
 			} else if (result.type === 'failure') {
 				submitter?.removeAttribute('disabled');
 				errors = result.data as ResetError;
@@ -42,21 +37,21 @@
 </script>
 
 <form method="POST" action="/auth?/reset" use:enhance={onSubmit} name="reset">
-    <input type='hidden' name='passwordResetToken' value={resetPasswordToken}/>
-  {#each errors?.server ?? [] as error} 
-        <span class='error' > {$LL.server[error as keyof typeof $LL.server]()}</span>
-    {/each}
+	<input type="hidden" name="passwordResetToken" value={resetPasswordToken} />
+	{#each errors?.server ?? [] as error}
+		<span class="error"> {$LL.server[error as keyof typeof $LL.server]()}</span>
+	{/each}
 
 	<label for="password">
 		{$LL.reset.password()}:
 	</label>
 	<input type="password" name="password" id="password" />
 
-     {#each errors?.password ?? [] as error} 
-        <span class='error' > {$LL.reset.errors[error as keyof typeof $LL.reset.errors]()}</span>
-    {/each}
+	{#each errors?.password ?? [] as error}
+		<span class="error"> {$LL.reset.errors[error as keyof typeof $LL.reset.errors]()}</span>
+	{/each}
 
-    <label for="passwordRepeat">
+	<label for="passwordRepeat">
 		{$LL.reset.passwordRepeat()}:
 	</label>
 	<input type="password" name="passwordRepeat" id="passwordRepeat" />
@@ -67,7 +62,7 @@
 </form>
 
 <style>
-   	form {
+	form {
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
@@ -75,3 +70,4 @@
 		width: 100%;
 	}
 </style>
+
--- src/routes/auth/+page.server.ts
+++ src/routes/auth/+page.server.ts
@@ -10,90 +10,90 @@ import { formDataToForgottenPasswordDTO } from '$lib/DTO/ForgottenPasswordDTO';
 import { formDataToResetPasswordDTO } from '$lib/DTO/ResetPasswordDTO';
 
 const loginAction: Action = async ({ cookies, request }) => {
-	const loginDTO = formDataToUserLoginDTO(await request.formData());
+    const loginDTO = formDataToUserLoginDTO(await request.formData());
 
-	if (loginDTO.type === 'error') {
-		return fail(400, loginDTO.value);
-	}
+    if (loginDTO.type === 'error') {
+        return fail(400, loginDTO.value);
+    }
 
-	const result = await login(loginDTO.value);
+    const result = await login(loginDTO.value);
 
-	if (result.type === 'error') {
-		return fail(400, result.errors);
-	}
+    if (result.type === 'error') {
+        return fail(400, result.errors);
+    }
 
-	const token = result.response.token;
-	cookies.set('jwt', token, { path: '/' });
+    const token = result.response.token;
+    cookies.set('jwt', token, { path: '/' });
 
-	axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
 
 const logoutAction: Action = ({ cookies }) => {
-	cookies.delete('jwt', { path: '/' });
-	axios.defaults.headers.common.Authorization = null;
+    cookies.delete('jwt', { path: '/' });
+    axios.defaults.headers.common.Authorization = null;
 
-	redirect(307, '/');
+    redirect(307, '/');
 };
 
 const registerAction: Action = async ({ request }) => {
-	const registerDTO = formDataToUserRegisterDTO(await request.formData());
+    const registerDTO = formDataToUserRegisterDTO(await request.formData());
 
-	if (registerDTO.type === 'error') {
-		return fail(400, { register: registerDTO.value });
-	}
+    if (registerDTO.type === 'error') {
+        return fail(400, { register: registerDTO.value });
+    }
 
-	const result = await register(registerDTO.value);
+    const result = await register(registerDTO.value);
 
-	if (result.type === 'error') {
-		return fail(400, { register: result.errors });
-	}
+    if (result.type === 'error') {
+        return fail(400, { register: result.errors });
+    }
 };
 
 const accountAction: Action = async ({ request, locals }) => {
-	const data = formDataToAccountChangeDTO(locals.user, await request.formData());
-	if (data.type === 'error') {
-		return fail(400, data.errors);
-	}
+    const data = formDataToAccountChangeDTO(locals.user, await request.formData());
+    if (data.type === 'error') {
+        return fail(400, data.errors);
+    }
 
-	const response = await accountChange(data.dto);
-	if (response.type === 'error') {
-		return fail(400, response.errors);
-	}
+    const response = await accountChange(data.dto);
+    if (response.type === 'error') {
+        return fail(400, response.errors);
+    }
 };
 
 const forgottenPasswordAction: Action = async ({ request }) => {
-	const formData = await request.formData();
-	const data = formDataToForgottenPasswordDTO(formData);
-	if (data.type === 'error') {
-		return fail(400, data.value);
-	}
+    const formData = await request.formData();
+    const data = formDataToForgottenPasswordDTO(formData);
+    if (data.type === 'error') {
+        return fail(400, data.value);
+    }
 
-	const response = await requestResetPassword(data.value, formData.get('lang')?.toString() ?? 'cs');
-	if (response.type === 'error') {
-		return fail(400, response.errors);
-	}
+    const response = await requestResetPassword(data.value, formData.get('lang')?.toString() ?? 'cs');
+    if (response.type === 'error') {
+        return fail(400, response.errors);
+    }
 };
 
 const resetAction: Action = async ({ request }) => {
-	const data = formDataToResetPasswordDTO(await request.formData());
-	if (data.type === 'error') {
-		return fail(400, data.value);
-	}
+    const data = formDataToResetPasswordDTO(await request.formData());
+    if (data.type === 'error') {
+        return fail(400, data.value);
+    }
 
-	const response = await resetPassword(data.value);
-	
-	if (response.type === 'error') {
-		return fail(400, response.errors);
-	}
-	
-	return redirect(307, '/');
+    const response = await resetPassword(data.value);
+
+    if (response.type === 'error') {
+        return fail(400, response.errors);
+    }
+
+    return redirect(307, '/');
 };
 
 export const actions: Actions = {
-	login: loginAction,
-	logout: logoutAction,
-	register: registerAction,
-	account: accountAction,
-	forgotten: forgottenPasswordAction,
-	reset: resetAction
+    login: loginAction,
+    logout: logoutAction,
+    register: registerAction,
+    account: accountAction,
+    forgotten: forgottenPasswordAction,
+    reset: resetAction
 };
