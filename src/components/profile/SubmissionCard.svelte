35f970a2231d68eee01a14f538dfd4ff5dfa6326
Jiri Velek <jiri.velek@yahoo.com>
2025-10-26 13:49:53 +0100

    remove jump from submission card

--- src/components/profile/SubmissionCard.svelte
+++ src/components/profile/SubmissionCard.svelte
@@ -148,11 +148,6 @@
 			box-shadow 0.25s ease;
 	}
 
-	.submission-card:hover {
-		transform: translateY(-6px);
-		box-shadow: 0 8px 24px rgba(0, 0, 0, 0.25);
-	}
-
 	.image-wrapper {
 		position: relative;
 	}
