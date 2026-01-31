698b8c6962ea7fad11ce88f183c1ff184a6e32b6
Jiri Velek <jiri.velek@yahoo.com>
2026-01-31 13:15:23 +0100

    reformat

--- src/routes/administration/season/+page.server.ts
+++ src/routes/administration/season/+page.server.ts
@@ -5,30 +5,30 @@ import { ArkErrors } from "arktype";
 import qs from "qs";
 
 const createAction: Action = async ({ request, locals }) => {
-	const formData = await request.formData();
+    const formData = await request.formData();
 
-	const value = qs.parse(new URLSearchParams(formData as any).toString());
-	const data = SeasonConfigType(value);
+    const value = qs.parse(new URLSearchParams(formData as any).toString());
+    const data = SeasonConfigType(value);
 
-	if (data instanceof ArkErrors) {
-		const errors = Object.fromEntries(data.map((err) => [err.path.toString(), err.message]));
+    if (data instanceof ArkErrors) {
+        const errors = Object.fromEntries(data.map((err) => [err.path.toString(), err.message]));
 
-		return fail(422, { errors });
-	}
+        return fail(422, { errors });
+    }
 
-	const result = await createSeason(locals.axios, data);
+    const result = await createSeason(locals.axios, data);
 
-	if (result.type === 'error') {
-		return fail(422, {
-			errors: Object.fromEntries(
-				Object.entries(result.errors).map(([key, value]) => [key, String(value)])
-			)
-		});
-	}
+    if (result.type === 'error') {
+        return fail(422, {
+            errors: Object.fromEntries(
+                Object.entries(result.errors).map(([key, value]) => [key, String(value)])
+            )
+        });
+    }
 
-	return redirect(307, '/administration/season');
+    return redirect(307, '/administration/season');
 }
 
 export const actions: Actions = {
-	create: createAction
+    create: createAction
 }
