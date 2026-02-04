424468f8d1c94b53f76dcbbd11e3fc3a18d96a23
Jiri Velek <jiri.velek@yahoo.com>
2026-02-04 22:50:10 +0100

    silence warnings for scss dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,23 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		paraglideVitePlugin({
-			project: './project.inlang',
-			outdir: './src/paraglide',
-			strategy: ['url', 'preferredLanguage', 'baseLocale']
-		}),
-		sveltekit()
-	],
-	server: {
-		port: 3000
-	}
+  plugins: [
+    paraglideVitePlugin({
+      project: './project.inlang',
+      outdir: './src/paraglide',
+      strategy: ['url', 'preferredLanguage', 'baseLocale']
+    }),
+    sveltekit()
+  ],
+  server: {
+    port: 3000,
+    allowedHosts: ['vytrvalec.local'],
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        quietDeps: true,
+      }
+    },
+  },
 });
