6ea2e09c3b9f0a31dfc9ff4b9a72693414041c7d
Alena Kašparová <a.kasp@seznam.cz>
2026-03-23 21:23:53 +0100

    fix: add missing page load

---
+++ src/routes/administration/season/[id=integer]/+page.ts
@@ -0,0 +1,10 @@
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async ({ parent, params }) => {
+    const id = Number(params.id);
+    const { seasons } = await parent();
+
+    return {
+        season: seasons.get(id) ?? null,
+    };
+};
