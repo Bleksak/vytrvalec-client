1a83248207d15ec63321e1385cd7547ce5be0bc6
Jiri Velek <jiri.velek@yahoo.com>
2026-05-21 23:53:34 +0200

    update eslint config to ignore auto generated files

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,16 @@
-import { defineConfig, globalIgnores } from 'eslint/config';
+import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
+    {
+        ignores: [
+            '.svelte-kit/**',
+            'src/paraglide/**',
+            'src/translations/**',
+        ],
+    },
     { files: ['src/**/*.{js,mjs,cjs,ts}'] },
     { files: ['src/**/*.{js,mjs,cjs,ts}'], languageOptions: { globals: globals.browser } },
     { files: ['src/**/*.{js,mjs,cjs,ts}'], plugins: { js }, extends: ['js/recommended'] },
